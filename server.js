0b5bf0ae247078ac1ae7e7de57adf2315a15accd
Kris Pennimpede <kpennimpede@hotmail.com>
2021-03-14 13:03:11 -0400

    basic working homepage that shows posts

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const path = require('path');
-const routes = require('./controllers')
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//handlebars initialization
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //use routes
 app.use(routes);
