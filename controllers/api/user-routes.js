fcda89c7189cafada35546c7546feea021dcba37
Kris Pennimpede <kpennimpede@hotmail.com>
2021-03-13 22:23:37 -0500

    create CRUD routes for Users

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,84 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// Get All users
+router.get('/', (req, res) => {
+  User.findAll({
+
+  })
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
+
+// Get a single user by id number
+router.get('/:id', (req, res) => {
+  User.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No User found with this id' });
+      return;
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
+
+//create a new user
+router.post('/', (req, res) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password
+  })
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
+
+//update a user by id number
+router.put('/:id', (req, res) => {
+  User.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData[0]) {
+      res.status(404).json({ message: 'No User found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
+
+//Delete a user by id number
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No User found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
+
+
+module.exports = router;
