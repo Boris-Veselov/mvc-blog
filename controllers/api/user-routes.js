d9aa5559c849f82faae6f5159224b9a59a35694f
Kris Pennimpede <kpennimpede@hotmail.com>
2021-03-14 21:04:46 -0400

    Fixed issue with withAuth function being called on user creation in error

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -45,7 +45,7 @@ router.get('/:id', (req, res) => {
 });
 
 //create a new user
-router.post('/', withAuth, (req, res) => {
+router.post('/', (req, res) => {
   User.create({
     username: req.body.username,
     password: req.body.password
